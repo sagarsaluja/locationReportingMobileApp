8cc3773bd800e1f3e18b887e861459042a3e351b
Sagar Saluja <sagar.saluja@onsolve.com>
2023-05-18 13:19:40 +0530

    changed to js from tsx

---
+++ LocationReporting/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => (<View>
+  <Text>{"this is a location reporting app"}</Text>
+</View>)
+export default App;
